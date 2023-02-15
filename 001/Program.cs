0f9d905b496bcfe10d8a08c880c1ff26954a1066
Rustam <rustamzakirov@mail.ru>
2023-02-15 21:39:06 +0300

    first

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,15 @@
-﻿int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
-Console.WriteLine($"[{string.Join(", ", numbers)}]");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.Write(count);
