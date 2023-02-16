59489a93ae72bd8190a23327ab00d2f9ab8a2000
Rustam <rustamzakirov@mail.ru>
2023-02-16 20:46:06 +0300

    second complete

---
+++ 002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координату b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+double x = (b2 - b1)/(k1-k2);
+double y = k1*x + b1;
+Console.WriteLine($"Координаты пересечения: x: {x} y: {y}");
